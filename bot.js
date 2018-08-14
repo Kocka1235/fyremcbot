576c36bc4a823e0286a274e4f0463ec2c9b52ea2
Kocka1235 <42372263+Kocka1235@users.noreply.github.com>
2018-08-14 13:20:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const botconfig = require("./botconfig.json");
 const tokenfile = require("./token.json");
 const Discord = require("discord.js");
-
+const token = process.env.token
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
@@ -32,12 +32,12 @@ bot.on("message", async message => {
     if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Ezt a személyt nem kickelheted!");
 
     let kickEmbed = new Discord.RichEmbed()
-    .setDescription("~Kick~")
-    .setColor("#e56b00")
-    .addField("Kickelt játékos:", `${kUser} with ID ${kUser.id}`)
-    .addField("Kickelte:", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Kickelte a csatornában:", message.channel)
-    .addField("Idő:", message.createdAt)
+    .setDescription("~Kick~");
+    .setColor("#e56b00");
+    .addField("Kickelt játékos:", `${kUser} with ID ${kUser.id}`);
+    .addField("Kickelte:", `<@${message.author.id}> with ID ${message.author.id}`);
+    .addField("Kickelte a csatornában:", message.channel);
+    .addField("Idő:", message.createdAt);
     .addField("Oka:", kReason);
 
     let kickChannel = message.guild.channels.find(`name`, "incidents");
@@ -54,16 +54,16 @@ bot.on("message", async message => {
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("Can't find user!");
     let bReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Ezt nem lehet!");
     if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Ezt a személyt nem kickelheted!");
 
     let banEmbed = new Discord.RichEmbed()
-    .setDescription("~Ban~")
-    .setColor("#bc0000")
-    .addField("Bannolt játékos:", `${bUser} with ID ${bUser.id}`)
-    .addField("Bannolta:", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Bannolás szobája:", message.channel)
-    .addField("Idő:", message.createdAt)
+    .setDescription("~Ban~");
+    .setColor("#bc0000");
+    .addField("Bannolt játékos:", `${bUser} with ID ${bUser.id}`);
+    .addField("Bannolta:", `<@${message.author.id}> with ID ${message.author.id}`);
+    .addField("Bannolás szobája:", message.channel);
+    .addField("Idő:", message.createdAt);
     .addField("Ban oka:", bReason);
 
     let incidentchannel = message.guild.channels.find(`name`, "incidents");
@@ -86,12 +86,12 @@ bot.on("message", async message => {
     let rreason = args.join(" ").slice(22);
 
     let reportEmbed = new Discord.RichEmbed()
-    .setDescription("Reports")
-    .setColor("#15f153")
-    .addField("Jelentett játékos:", `${rUser} with ID: ${rUser.id}`)
-    .addField("Jelentette:", `${message.author} with ID: ${message.author.id}`)
-    .addField("Szoba:", message.channel)
-    .addField("Ideje:", message.createdAt)
+    .setDescription("Reports");
+    .setColor("#15f153");
+    .addField("Jelentett játékos:", `${rUser} with ID: ${rUser.id}`);
+    .addField("Jelentette:", `${message.author} with ID: ${message.author.id}`);
+    .addField("Szoba:", message.channel);
+    .addField("Ideje:", message.createdAt);
     .addField("Oka:", rreason);
 
     let reportschannel = message.guild.channels.find(`name`, "reports");
@@ -111,12 +111,12 @@ bot.on("message", async message => {
 
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
-    .setDescription("Szerver információk")
-    .setColor("#15f153")
-    .setThumbnail(sicon)
-    .addField("Szerver neve:", message.guild.name)
-    .addField("Készült:", message.guild.createdAt)
-    .addField("Te Csatlakoztál:", message.member.joinedAt)
+    .setDescription("Szerver információk");
+    .setColor("#15f153");
+    .setThumbnail(sicon);
+    .addField("Szerver neve:", message.guild.name);
+    .addField("Készült:", message.guild.createdAt);
+    .addField("Te Csatlakoztál:", message.member.joinedAt);
     .addField("Játékosok száma:", message.guild.memberCount);
 
     return message.channel.send(serverembed);
@@ -128,10 +128,10 @@ bot.on("message", async message => {
 
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
-    .setDescription("Bot információk")
-    .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Bot neve:", bot.user.username)
+    .setDescription("Bot információk");
+    .setColor("#15f153");
+    .setThumbnail(bicon);
+    .addField("Bot neve:", bot.user.username);
     .addField("Készült:", bot.user.createdAt);
 
     return message.channel.send(botembed);
