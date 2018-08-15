757da48eade3fc304de0d05885fe3606bada3851
Kocka1235 <42372263+Kocka1235@users.noreply.github.com>
2018-08-15 10:44:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+    client.user.setActivity('Fyremc', {type: 'Játszás'});
 });
 
 client.on('message', msg => {
